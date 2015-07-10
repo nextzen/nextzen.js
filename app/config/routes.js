f17fa3ebde77734e815db782496409159353ef9c
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2015-07-10 10:50:32 -0400

    macs filesystems are case insensitive ;(

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var Main = require('../components/Main');
+var Main = require('../Components/Main');
 //var Home = require('../componenets/Home');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
