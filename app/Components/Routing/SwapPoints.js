c494675fe5fcfd32adad37d9551283f19e51d9b4
hanbyul-here <hanbyul@nyu.edu>
2015-08-12 18:49:09 -0400

    adding start/dest swapping function

--- app/Components/Routing/SwapPoints.js
+++ app/Components/Routing/SwapPoints.js
@@ -1,10 +1,15 @@
 var React = require('react');
 
 var SwapPoints = React.createClass({
+  swapPoints: function(){
+    var tempPoint = this.props.startPoint;
+    this.props.setStartPoint(this.props.destPoint);
+    this.props.setDestPoint(tempPoint);
+  },
   render: function(){
     return (
-    <div className = "swapPoints">
-    </div>
+    <div className = "swapPoints"
+          onClick= {this.swapPoints} />
     );
   }
 })
