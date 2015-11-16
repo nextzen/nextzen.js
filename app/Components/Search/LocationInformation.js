5ebab53f641a6fb0ec29c11dd8ffc9139a4f0391
hanbyul-here <hanbyul@nyu.edu>
2015-11-16 17:09:00 -0500

    fitting url parsing into react component lifecycle

--- app/Components/Search/LocationInformation.js
+++ app/Components/Search/LocationInformation.js
@@ -11,52 +11,67 @@ var Keys = require('../Keys.js');
 var $ = require('jquery');
 
 var LocationInformation = React.createClass({
-  render: function(){
-
-    var mainTitle = "";
-    var neighborhood = "";
-
+  
+  getInitialState: function(){    
+    var _mainTitle = "";
+    var _neighborhood = "";
     if((Object.keys(this.props.selectedPoint).length !== 0)) {
       var title = this.props.selectedPoint.name.split(',');
-      mainTitle = title[0];
-      neighborhood = title[1] + title [2];
-    } else {
+      _mainTitle = title[0];
+      _neighborhood = title[1] + title [2];
+    } 
+    return {
+      mainTitle: _mainTitle,
+      neighborhood: _neighborhood
+    }
+  },
+
+
+  componentDidMount: function() {
+    if((Object.keys(this.props.location.query).length !== 0)) {
       var gid = this.props.location.query.gid;
-      var callurl = 'https://search.mapzen.com/v1/place?api_key='
+      var callurl = 'https://search.mapzen.com/v1/place?api_key=';
       var apikey = Keys.search;
       callurl += apikey;
       callurl +='&ids=';
-      callurl += gid;
+      callurl += gid; 
       var self = this;
       
       $.ajax({
         type:"GET",
         crossDomain: true,
-          url: callurl,
-          success: function(data){
+        url: callurl,
+        success: function(data){
             var title = data.features[0].properties.label.split(',');
-            mainTitle = title[0];
-            neighborhood = title[1] + title[2];
-
+            var _mainTitle = title[0];
+            var _neighborhood = title[1] + title[2];
 
             self.props.setDestinationPoint({
-              name: mainTitle,
+              name: _mainTitle,
               lat: data.features[0].geometry.coordinates[0],
               lon: data.features[0]. geometry.coordinates[1]
             });
+          self.setState({
+            mainTitle: _mainTitle,
+            neighborhood: _neighborhood
+          }, function() {
             document.getElementById('locationTitle').innerHTML = mainTitle;
             document.getElementById('locationNeighborhood').innerHTML = neighborhood;
-          },
-          error: function(){
+          });
+        },
+        error: function(){
             console.log('can\'t find the place');
-          }
+        }
       });
-
     }
+  },
+
+  render: function(){
+
     return(
       <div className = "locationInformation">
-        <div id = "locationTitle" className = "locationTitle"> {mainTitle} </div>
-        <div id = "locationNeighborhood" className = "neighborhood"> {neighborhood} </div>
+        <div id = "locationTitle" className = "locationTitle"> {this.state.mainTitle} </div>
+        <div id = "locationNeighborhood" className = "neighborhood"> {this.state.neighborhood} </div>
         <RouteButton />
       </div>
     );
