0063c8fb33c0b97beb575784a1edd17731969064
hanbyul-here <hanbyul@nyu.edu>
2015-11-16 17:09:31 -0500

    making Search Button independant

--- app/Components/Search/SearchButton.js
+++ app/Components/Search/SearchButton.js
@@ -3,15 +3,21 @@ var React = require('react');
 var Actions = require('../../actions');
 var store = require('../../reducer');
 
+import { Link } from 'react-router';
+
 var SearchButton = React.createClass({
+
   setMode : function(){
     store.dispatch(Actions.setMapModeAction('search'));
   },
+
   render : function(){
     return(
-      <div className="searchButton" onClick = {this.setMode}> 
-        <span className = "searchPlaceholder"> Search addres or place. </span>
+      <Link to = '/maps/search'> 
+        <div className="searchButton" onClick = {this.setMode}> 
+          <span className = "searchPlaceholder"> Search addres or place. </span>
         </div>
+      </Link>
     );
   }
 });
