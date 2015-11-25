47852fa446d554b0649d69808af6da98ee0795a9
hanbyul-here <hanbyul@nyu.edu>
2015-11-25 11:17:17 -0500

    cancle button clears the map

--- app/Components/Search/CancelButton.js
+++ app/Components/Search/CancelButton.js
@@ -2,10 +2,12 @@ var React = require('react');
 var Actions = require('../../actions');
 var store = require('../../reducer');
 import { Link } from 'react-router';
+import MapObject from '../Map/MapObject';
 
 var CancelButton = React.createClass({
   setMode : function(){
     store.dispatch(Actions.clearPointsAction());
+    MapObject.clearMap();
   },
   render : function(){
     return(
