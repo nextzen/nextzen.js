465367942f46ddb7a8f36ce36b4e3f57ccfa7509
binx <rachelbinx@gmail.com>
2016-04-24 21:16:57 -0700

    leaflet test

--- test/test.js
+++ test/test.js
@@ -1,14 +1,25 @@
-// Use require only if available (ran from Node)
-// if (typeof require == 'function') {
-//     var assert = require('assert'),
-//     L = require('leaflet');
-//     YourApp = require('./../index');
-// }
-
-
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 var jsdom = require('mocha-jsdom');
+var happen = require('happen');
 var WebMap = require('./../index');
+
+describe('web-map-stateful-url', function(){
+  jsdom();
+  var map;
+
+  beforeEach(function() {
+    var L = require('leaflet');
+    map = new L.Map(document.createElement('div'));
+  });
+
+  it('check that zoom is being set',function(){
+    map.setView([51.505, -0.09], 13);
+    expect(map.getZoom()).to.eq(13);
+    console.log(WebMap.draw())
+  });
+
+});
 
 
 describe('leaflet', function() {
@@ -17,16 +28,5 @@ describe('leaflet', function() {
     var L = require('leaflet');
     console.log(WebMap);
     expect(L.version).to.eq('0.7.7');
-  });
-});
-
-
-
-describe('test', function() {
-  jsdom();
-  it('tests just to see how DOM works', function() {
-    WebMap.domTest();
-    var testEl = document.querySelector('#testEl');
-    expect(testEl.innerHTML).to.equal('Hello world');
   });
 });
