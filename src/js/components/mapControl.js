46b427e9f6b970c19cbfffd03f84d1eced024412
Hanbyul Jo <hanbyul@nyu.edu>
2016-06-16 15:43:12 -0400

    scene fix

--- src/js/components/mapControl.js
+++ src/js/components/mapControl.js
@@ -24,27 +24,18 @@ var MapControl = L.Map.extend({
     if (this.options.scene) {
       if (this._hasWebGL()) {
         switch (this.options.scene) {
+          // Set up Mapzen house style
           case 'zinc':
-            Tangram.leafletLayer({
-              scene: 'https://mapzen.com/carto/zinc-style/zinc-style.yaml',
-              attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
-            }).addTo(this);
-            break;
           case 'refill':
+          case 'cinnabar':
             Tangram.leafletLayer({
-              scene: 'https://mapzen.com/carto/refill-style/refill-style.yaml',
+              scene: 'https://mapzen.com/carto/' + this.options.scene + '-style/' + this.options.scene + '-style.yaml',
               attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
             }).addTo(this);
             break;
           case 'bubble-wrap':
             Tangram.leafletLayer({
-              scene: 'https://mapzen.com/carto/bubble-wrap-style/2/bubble-wrap.yaml',
-              attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
-            });
-            // break;
-          case 'cinnabar':
-            Tangram.leafletLayer({
-              scene: 'https://mapzen.com/carto/cinnabar-style/cinnabar-style.yaml',
+              scene: 'https://mapzen.com/carto/' + this.options.scene + '-style/' + this.options.scene + '.yaml',
               attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
             }).addTo(this);
             break;
@@ -53,6 +44,7 @@ var MapControl = L.Map.extend({
               scene: this.options.scene,
               attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
             }).addTo(this);
+            break;
         }
       } else {
         console.log('WebGL is not available, falling back to OSM default tile.');
