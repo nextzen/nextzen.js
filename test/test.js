0d703609b32865fea5b1b60e3bdac4e6f9a0caf8
Hanbyul Jo <hanbyul@nyu.edu>
2016-06-21 14:41:48 -0400

    adding test for tangram layer

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 describe('leaflet', function () {
   var el;
-  var webmap;
+  var map;
   var windowHistory;
 
   before('initialize map', function () {
@@ -10,20 +10,41 @@ describe('leaflet', function () {
     el.style.cssText = 'position: absolute; left: 0; top: 0; width: 100%; height: 100%;';
     document.body.appendChild(el);
     windowHistory = window.history;
-    webmap = L.Mapzen.map(el);
-    webmap.setView([51.505, -0.09], 13);
+    map = L.Mapzen.map(el);
+    map.setView([51.505, -0.09], 13);
   });
 
   it('check which Leaflet version it is', function () {
     expect(L.version).to.equal('0.7.7');
   });
 
+
+  it("checks default style is set.", function(done){
+
+    // Give time to load Tangram script
+    setTimeout(function() {
+      if (map._hasWebGL()) {
+        var tangramLayer = false;
+
+        map.eachLayer( function(layer) {
+          if (layer.scene) tangramLayer = true;
+        });
+
+        if(tangramLayer) done();
+        else done(err);
+
+      } else {
+        done();
+      }
+    }, 1500);
+  });
+
   it('checks that hash for coord is working', function () {
     L.Mapzen.hash({
-      map: webmap
+      map: map
     });
-    var zoom = webmap.getZoom();
-    var center = webmap.getCenter();
+    var zoom = map.getZoom();
+    var center = map.getCenter();
 
     var getPrecision = function (z) {
       return Math.max(0, Math.ceil(Math.log(z) / Math.LN2));
@@ -36,7 +57,7 @@ describe('leaflet', function () {
   });
 
   it('checks that states are not pushed to history', function () {
-    webmap.setView([51.505, -2.09], 13);
+    map.setView([51.505, -2.09], 13);
     expect(window.history).to.equal(windowHistory);
   });
 });
