c4021acd0ab729d4ec3401a2d05bc7c857dace8b
Michal Migurski <mike@teczno.com>
2016-06-29 11:50:37 -0700

    Fixed some path names in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,14 @@ VMINOR=`cut -d. -f1,2 VERSION`
 VMAJOR=`cut -d. -f1 VERSION`
 
 if aws s3 ls "s3://${BUCKET}/js/${VPATCH}/mapzen.min.js"; then
-    echo "s3://${BUCKET}/js/${VPATCH}/mapzen.min.js already exits"
-    aws s3 cp --recursive "s3://static-prod.mapzen.com/js/${VPATCH}" "live-{$VPATCH}"
+    echo "s3://${BUCKET}/js/${VPATCH}/mapzen.min.js already exits, checking diffs..."
+    aws s3 cp --recursive --quiet "s3://static-prod.mapzen.com/js/${VPATCH}" "live-${VPATCH}"
 
     for NAME in mapzen.min.js mapzen.js mapzen.css images; do
-        if diff -r "dist/${NAME}" "live-{$VPATCH}/${NAME}"; then
-            echo "No differences between dist/${NAME} and live-{$VPATCH}/${NAME}"
+        if diff -r "dist/${NAME}" "live-${VPATCH}/${NAME}"; then
+            echo "No differences between dist/${NAME} and live-${VPATCH}/${NAME}"
         else
-            echo "Found a difference between dist/${NAME} and live-{$VPATCH}/${NAME}"
+            echo "Found a difference between dist/${NAME} and live-${VPATCH}/${NAME}"
             exit 1
         fi
     done
