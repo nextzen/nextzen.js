02c8d5c7eafe37bf8c84a1c91652e6af94ed9e44
Hanbyul Jo <hanbyul@nyu.edu>
2016-08-02 16:00:18 -0400

    made attributionText compatible

--- src/js/components/mapControl.js
+++ src/js/components/mapControl.js
@@ -20,7 +20,8 @@ var MapControl = L.Map.extend({
     // Adding Mapzen attribution to Leaflet
     if (this.attributionControl) {
       this.attributionControl.setPrefix('');
-      this.attributionControl.addAttribution(this.options.attribution);
+      var tempAttr = this.options.attributionText || this.options.attribution;
+      this.attributionControl.addAttribution(tempAttr);
       this.attributionControl.addAttribution('<a href="http://leafletjs.com/">Leaflet</a>');
     }
     // Set Icon path manually
