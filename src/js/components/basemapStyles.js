52a03c0c6cbc6ed88a70eacdda113d6bf269fcf7
Nathaniel Kelso <nvkelso@gmail.com>
2016-08-18 15:09:52 -0700

    point at major versions of Mapzen house styles (not unversioned latest)

--- src/js/components/basemapStyles.js
+++ src/js/components/basemapStyles.js
@@ -1,18 +1,18 @@
 // Mapzen House style for Tangram
 var style = {
-  BubbleWrap: 'https://mapzen.com/carto/bubble-wrap-style/bubble-wrap.yaml',
-  Cinnabar: 'https://mapzen.com/carto/cinnabar-style/cinnabar-style.yaml',
-  CinnabarMoreLabels: 'https://mapzen.com/carto/cinnabar-style-more-labels/cinnabar-style-more-labels.yaml',
-  CinnabarNoLabels: 'https://mapzen.com/carto/cinnabar-style-no-labels/cinnabar-style-no-labels.yaml',
-  Refill: 'https://mapzen.com/carto/refill-style/refill-style.yaml',
-  RefillMoreLabels: 'https://mapzen.com/carto/refill-style-more-labels/refill-style-more-labels.yaml',
-  RefillNoLabels: 'https://mapzen.com/carto/refill-style-no-labels/refill-style-no-labels.yaml',
-  Zinc: 'https://mapzen.com/carto/zinc-style/zinc-style.yaml',
-  ZincMoreLabels: 'https://mapzen.com/carto/zinc-style-more-labels/zinc-style-more-labels.yaml',
-  ZincNoLabels: 'https://mapzen.com/carto/zinc-style-no-labels/zinc-style-no-labels.yaml',
-  Walkabout: 'https://mapzen.com/carto/walkabout-style/walkabout-style.yaml',
-  WalkaboutMoreLabels: 'https://mapzen.com/carto/walkabout-style-more-labels/walkabout-style-more-labels.yaml',
-  WalkaboutNoLabels: 'https://mapzen.com/carto/walkabout-style-no-labels/walkabout-style-no-labels.yaml'
+  BubbleWrap: 'https://mapzen.com/carto/bubble-wrap-style/4/bubble-wrap.yaml',
+  Cinnabar: 'https://mapzen.com/carto/cinnabar-style/3/cinnabar-style.yaml',
+  CinnabarMoreLabels: 'https://mapzen.com/carto/cinnabar-style-more-labels/3/cinnabar-style-more-labels.yaml',
+  CinnabarNoLabels: 'https://mapzen.com/carto/cinnabar-style-no-labels/3/cinnabar-style-no-labels.yaml',
+  Refill: 'https://mapzen.com/carto/refill-style/3/refill-style.yaml',
+  RefillMoreLabels: 'https://mapzen.com/carto/refill-style-more-labels/3/refill-style-more-labels.yaml',
+  RefillNoLabels: 'https://mapzen.com/carto/refill-style-no-labels/3/refill-style-no-labels.yaml',
+  Zinc: 'https://mapzen.com/carto/zinc-style/3/zinc-style.yaml',
+  ZincMoreLabels: 'https://mapzen.com/carto/zinc-style-more-labels/3/zinc-style-more-labels.yaml',
+  ZincNoLabels: 'https://mapzen.com/carto/zinc-style-no-labels/3/zinc-style-no-labels.yaml',
+  Walkabout: 'https://mapzen.com/carto/walkabout-style/1/walkabout-style.yaml',
+  WalkaboutMoreLabels: 'https://mapzen.com/carto/walkabout-style-more-labels/1/walkabout-style-more-labels.yaml',
+  WalkaboutNoLabels: 'https://mapzen.com/carto/walkabout-style-no-labels/1/walkabout-style-no-labels.yaml'
 };
 
 module.exports = style;
