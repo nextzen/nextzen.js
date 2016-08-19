0cf286523e8cfa914c42aac451bb294395cd7add
Hanbyul Jo <hanbyul@nyu.edu>
2016-08-19 15:38:10 -0400

    added test coverage check

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -7,28 +7,30 @@ module.exports = function (config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
-
     // list of files / patterns to load in the browser
     files: [
       // test dependancies
       'node_modules/sinon/pkg/sinon.js',
       'node_modules/expect.js/index.js',
       'node_modules/happen/happen.js',
-      'dist/mapzen.js',
+      'src/js/*.js',
+      'src/js/**/*.js',
       'test/spec/mapControl.js',
       'test/spec/hash.js'
     ],
 
     plugins: [
       'karma-mocha',
+      'karma-browserify',
       'karma-coverage',
       'karma-coveralls',
       'karma-phantomjs-launcher',
       'karma-browserstack-launcher'
     ],
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['browserify', 'mocha'],
     // list of files to exclude
     exclude: [],
 
@@ -41,18 +43,30 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'js/*.js': ['coverage']
+      'src/**/*.js': ['browserify'],
+      'test/**/*.spec.js': ['browserify']
+    },
+
+    browserify: {
+      debug: true,
+      transform: [ ['babelify', {
+        presets: 'es2015'}], ['browserify-istanbul', {
+          instrumenterConfig: {
+            embedSource: true
+          }
+        }]
+      ]
     },
 
     coverageReporter: {
-      type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
+      type: 'html', // lcov or lcovonly are required for generating lcov.info files
       dir: 'coverage/'
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
