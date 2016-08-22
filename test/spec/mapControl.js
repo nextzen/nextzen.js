d6bd7f5d93a6040f0ac77ea402bfb6a865b4dd94
Hanbyul Jo <hanbyul@nyu.edu>
2016-08-22 17:12:35 -0400

    added test for Tangram Event

--- test/spec/mapControl.js
+++ test/spec/mapControl.js
@@ -1,47 +1,67 @@
 describe('Map Control Test', function () {
-  var el;
 
-  beforeEach(function () {
+  var el;
+  var testMap;
+  var spy;
+
+
+  var _hasWebGL = function() {
+    try {
+      var canvas = document.createElement('canvas');
+      return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+    } catch (x) {
+      return false;
+    }
+  }
+
+
+  before(function () {
     el = document.createElement('div');
     el.style.cssText = 'position: absolute; left: 0; top: 0; width: 100%; height: 100%;';
     document.body.appendChild(el);
+    testMap = L.Mapzen.map(el);
+    spy = sinon.spy();
+    testMap.addEventListener('tangramloaded', spy);
+    testMap.setView([51.505, -0.09], 13);
+
   });
 
-  afterEach(function () {
+  after(function () {
     el.parentNode.removeChild(el);
   })
 
-  describe('Tangram check', function () {
+  describe('Tangram layer check', function () {
     it('checks default style is set.', function (done) {
-      var _hasWebGL = function() {
-        try {
-          var canvas = document.createElement('canvas');
-          return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
-        } catch (x) {
-          return false;
-        }
-      }
-      var thisMap = L.Mapzen.map(el);
+
       // Give time to load Tangram script
       var count = 0;
-
       var checkTangramLayer = function () {
         var tangramLayer;
-        thisMap.eachLayer(function (layer) {
+        testMap.eachLayer(function (layer) {
           if (layer.scene) tangramLayer = layer;
         });
         count++;
         if (_hasWebGL()) {
-          if (tangramLayer === undefined && count < 20) return setTimeout(checkTangramLayer.bind(this), 200);
+          if (tangramLayer === undefined && count < 40) return setTimeout(checkTangramLayer.bind(this), 200);
           else if (tangramLayer) done();
-          else if (count >= 20) done(new Error('takes too long to load Tangram'))
+          else if (count >= 40) done(new Error('takes too long to load Tangram'))
         } else {
           done();
         }
       }
-
       checkTangramLayer();
-
     });
+
+    it('checks Tangram Event', function (done) {
+      var tangramEventCheck = function () {
+        if(_hasWebGL())
+          if(spy.called) done();
+          else return setTimeout(tangramEventCheck.bind(this), 200);
+        else done();
+      }
+      tangramEventCheck();
+    })
+
   })
+
 });
