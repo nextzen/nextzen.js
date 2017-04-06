315304393619370cb7b2042ac1ba1f10c44d4997
Hanbyul Jo <hanbyul@nyu.edu>
2017-04-06 14:39:43 -0400

    added check the case that key is not passed to search component

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -3,7 +3,13 @@ var L = require('leaflet');
 var Geocoder = require('leaflet-geocoder-mapzen/src/core');
 
 module.exports = Geocoder;
-module.exports.geocoder = function(options) {
-  var key = L.Mapzen.apiKey || options.key;
-  return new Geocoder(key, options);
+module.exports.geocoder = function(key, options) {
+  // When the key is omitted and options was passed
+  var apiKey;
+  if (typeof key !== 'string') {
+    apiKey = L.Mapzen.apiKey;
+  } else {
+    apiKey = key;
+  }
+  return new Geocoder(apiKey, options);
 }
