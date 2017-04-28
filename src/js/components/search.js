558af044c330536b6e401cf81ccb160c4a336118
Hanbyul Jo <hanbyul@nyu.edu>
2017-04-28 10:21:17 -0400

    checked the case when key is obmitted

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -4,10 +4,16 @@ var Geocoder = require('leaflet-geocoder-mapzen/src/core');
 
 module.exports = Geocoder;
 module.exports.geocoder = function (key, options) {
-  // When the key is omitted and options was passed
+
   var apiKey;
-  if (typeof key !== 'string') {
+
+  if (typeof key !== 'string' && typeof key !== 'object') {
+    // When nothing is passed
     apiKey = L.Mapzen.apiKey;
+  } else if (typeof key == 'object') {
+    // When the key is omitted and options is passed
+    apiKey = L.Mapzen.apiKey;
+    options = key;
   } else {
     apiKey = key;
   }
