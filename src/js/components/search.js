10b49d89e900aec4a1316c51a48b47d126d1b382
Hanbyul Jo <hanbyul@nyu.edu>
2017-05-11 18:52:43 -0400

    made syntax simpler

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -16,14 +16,12 @@ module.exports.geocoder = function (_key, _options) {
     // When the key is omitted and options is passed
     apiKey = L.Mapzen.apiKey;
     options = _key;
-    if (options.attribution) attribution += options.attribution;
-
   } else {
     apiKey = _key;
     options = _options;
-    if (options.attribution) attribution += options.attribution;
   }
 
+  if (options.attribution) attribution += options.attribution;
   options.attribution = attribution;
 
   if (!APIKeyCheck.isValidMapzenApiKey(apiKey)) {
