e2357c8f2a2f8c71f4e72bdda17272bd1e1e5f6c
Hanbyul Jo <hanbyul@nyu.edu>
2017-09-01 15:14:00 -0400

    started working on putting search methods back

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,5 +1,57 @@
 var APIKeyCheck = require('./apiKeyCheck');
 var Geocoder = require('leaflet-geocoder-mapzen/src/core');
+var corslite = require('@mapbox/corslite');
+
+Geocoder.prototype.serialize = function (params) {
+  var data = '';
+
+  for (var key in params) {
+    if (params.hasOwnProperty(key)) {
+      var param = params[key];
+      var type = param.toString();
+      var value;
+
+      if (data.length) {
+        data += '&';
+      }
+
+      switch (type) {
+        case '[object Array]':
+          value = (param[0].toString() === '[object Object]') ? JSON.stringify(param) : param.join(',');
+          break;
+        case '[object Object]':
+          value = JSON.stringify(param);
+          break;
+        case '[object Date]':
+          value = param.valueOf();
+          break;
+        default:
+          value = param;
+          break;
+      }
+
+      data += encodeURIComponent(key) + '=' + encodeURIComponent(value);
+    }
+  }
+  return data;
+};
+
+Geocoder.prototype.getSearchResult = function (input, callback) {
+  var param = {
+    text: input
+  };
+  var params = this.getParams(param);
+  corslite(this.options.url + '/search?' + this.serialize(params), callback, true);
+};
+
+Geocoder.prototype.getAutoCompleteResult = function (input, callback) {
+  var param = {
+    text: input
+  };
+
+  var params = this.getParams(param);
+  corslite(this.options.url + '/autocomplete?' + this.serialize(params), callback, true);
+};
 
 module.exports = Geocoder;
 
