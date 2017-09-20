16ebbc97874204a838b65091f2b45d76948d7006
Nathaniel V. KELSO <nvkelso@gmail.com>
2017-09-20 15:15:59 -0700

    Update basemapStyles.js

--- src/js/components/basemapStyles.js
+++ src/js/components/basemapStyles.js
@@ -1,23 +1,23 @@
 // Mapzen House style for Tangram
 var style = {
-  BubbleWrap: 'https://mapzen.com/carto/bubble-wrap-style/7/bubble-wrap-style.zip',
-  BubbleWrapMoreLabels: 'https://mapzen.com/carto/bubble-wrap-style-more-labels/7/bubble-wrap-style-more-labels.zip',
-  BubbleWrapNoLabels: 'https://mapzen.com/carto/bubble-wrap-style-no-labels/7/bubble-wrap-style-no-labels.zip',
-  Cinnabar: 'https://mapzen.com/carto/cinnabar-style/7/cinnabar-style.zip',
-  CinnabarMoreLabels: 'https://mapzen.com/carto/cinnabar-style-more-labels/7/cinnabar-style-more-labels.zip',
-  CinnabarNoLabels: 'https://mapzen.com/carto/cinnabar-style-no-labels/7/cinnabar-style-no-labels.zip',
-  Refill: 'https://mapzen.com/carto/refill-style/7/refill-style.zip',
-  RefillMoreLabels: 'https://mapzen.com/carto/refill-style-more-labels/7/refill-style-more-labels.zip',
-  RefillNoLabels: 'https://mapzen.com/carto/refill-style-no-labels/7/refill-style-no-labels.zip',
-  Zinc: 'https://mapzen.com/carto/zinc-style/6/zinc-style.zip',
-  ZincMoreLabels: 'https://mapzen.com/carto/zinc-style-more-labels/6/zinc-style-more-labels.zip',
-  ZincNoLabels: 'https://mapzen.com/carto/zinc-style-no-labels/6/zinc-style-no-labels.zip',
-  Walkabout: 'https://mapzen.com/carto/walkabout-style/5/walkabout-style.zip',
-  WalkaboutMoreLabels: 'https://mapzen.com/carto/walkabout-style-more-labels/5/walkabout-style-more-labels.zip',
-  WalkaboutNoLabels: 'https://mapzen.com/carto/walkabout-style-no-labels/5/walkabout-style-no-labels.zip',
-  Tron: 'https://mapzen.com/carto/tron-style/4/tron-style.zip',
-  TronMoreLabels: 'https://mapzen.com/carto/tron-style-more-labels/4/tron-style-more-labels.zip',
-  TronNoLabels: 'https://mapzen.com/carto/tron-style-no-labels/4/tron-style-no-labels.zip'
+  BubbleWrap: 'https://mapzen.com/carto/bubble-wrap-style/8/bubble-wrap-style.zip',
+  BubbleWrapMoreLabels: { 'https://mapzen.com/carto/bubble-wrap-style/8/bubble-wrap-style.zip', 'https://mapzen.com/carto/bubble-wrap-style/8/themes/label-10.zip'} ,
+  BubbleWrapNoLabels: { 'https://mapzen.com/carto/bubble-wrap-style/8/bubble-wrap-style.zip', 'https://mapzen.com/carto/bubble-wrap-style/8/themes/label-0.zip'} ,
+  Cinnabar: 'https://mapzen.com/carto/cinnabar-style/8/cinnabar-style.zip',
+  CinnabarMoreLabels: { 'https://mapzen.com/carto/cinnabar-style/8/cinnabar-style.zip', 'https://mapzen.com/carto/cinnabar-style/8/themes/label-10.zip' },
+  CinnabarNoLabels: { 'https://mapzen.com/carto/cinnabar-style/8/cinnabar-style.zip', 'https://mapzen.com/carto/cinnabar-style/8/themes/label-0.zip' },
+  Refill: 'https://mapzen.com/carto/refill-style/9/refill-style.zip',
+  RefillMoreLabels: { 'https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/label-10.zip' },
+  RefillNoLabels: { 'https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/label-0.zip' },
+  Zinc: { 'https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/color-zinc.zip' },
+  ZincMoreLabels: { 'https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/color-zinc.zip', 'https://mapzen.com/carto/refill-style/9/themes/label-10.zip' },
+  ZincNoLabels: { 'https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/color-zinc.zip', 'https://mapzen.com/carto/refill-style/9/themes/label-0.zip' },
+  Walkabout: 'https://mapzen.com/carto/walkabout-style/6/walkabout-style.zip',
+  WalkaboutMoreLabels: { 'https://mapzen.com/carto/walkabout-style/6/walkabout-style.zip', 'https://mapzen.com/carto/walkabout-style/6/themes/label-10.zip' },
+  WalkaboutNoLabels: { 'https://mapzen.com/carto/walkabout-style/6/walkabout-style.zip', 'https://mapzen.com/carto/walkabout-style/6/themes/label-0.zip' },
+  Tron: 'https://mapzen.com/carto/tron-style/5/tron-style.zip',
+  TronMoreLabels: { 'https://mapzen.com/carto/tron-style/5/tron-style.zip', 'https://mapzen.com/carto/tron-style/5/themes/label-10.zip' },
+  TronNoLabels: { 'https://mapzen.com/carto/tron-style/5/tron-style.zip', 'https://mapzen.com/carto/tron-style/5/themes/label-0.zip' }
 };
 
 module.exports = style;
