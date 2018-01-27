349b4004b861a86a79242e940079b6ee71aacabb
hanbyul_here <hanbyul@nyu.edu>
2018-01-27 16:57:55 -0500

    made house steyls to point to nextzen.org

--- src/js/components/basemapStyles.js
+++ src/js/components/basemapStyles.js
@@ -1,29 +1,121 @@
-var getImportObject = function (sceneArray) {
+/**
+ * Javascript native functions to extend object (Object.assign) does not work with getter
+ * extend() returns newly crated object based on source, with target object's values
+ * target object should be flat
+ */
+function extend (target, source) {
+  var extendedObj = Object.create(source);
+  for (var e in target) {
+    extendedObj[e] = target[e];
+  }
+  return extendedObj;
+}
+
+function getImportObject (sceneArray) {
   return {
     import: sceneArray
   };
-};
+}
 
-// Mapzen House style for Tangram
+function getCartoBaseStyleURL (cartoStyle) {
+  return cartoStyle.baseURL + '/' + cartoStyle.stylePath + '/' + cartoStyle.version + '/' + cartoStyle.stylePath + '.' + cartoStyle.suffix;
+}
+
+function getCustomLabelStyleURL (cartoStyle, labelPath, customValue) {
+  return cartoStyle.baseURL + '/' + cartoStyle.stylePath + '/' + 'themes' + '/' + labelPath + '-' + customValue + '.' + cartoStyle.suffix;
+}
+
+function getCartoThemeStyleURL (cartoStyle, customTheme) {
+  return cartoStyle.baseURL + '/' + cartoStyle.stylePath + '/' + 'themes' + '/' + customTheme + '.' + cartoStyle.suffix;
+}
+
+/**
+ * CartoStyle contains the common elements for all basemap styles
+ * update this only when there is a change that needs to happen across all styles
+ */
+var CartoStyle = {
+  baseURL: 'https://www.nextzen.com/carto',
+  suffix: 'zip',
+  moreLabelNumber: 10,
+  noLabelNumber: 0,
+  get basemapURL () {
+    return getCartoBaseStyleURL(this);
+  },
+  get moreLabelURL () {
+    return getCustomLabelStyleURL(this, 'label', this.moreLabelNumber);
+  },
+  get noLabelURL () {
+    return getCustomLabelStyleURL(this, 'label', this.noLabelNumber);
+  },
+  get moreLabelConfig () {
+    return getImportObject([this.basemapURL, this.moreLabelURL]);
+  },
+  get noLabelConfig () {
+    return getImportObject([this.basemapURL, this.noLabelURL]);
+  }
+};
+/**
+ * Change the value for veresion below for each style's new release
+ */
+var bubblewrap = extend({
+  stylePath: 'bubble-wrap-style',
+  version: 9
+}, CartoStyle);
+
+var cinnabar = extend({
+  stylePath: 'cinnabar-style',
+  version: 9
+}, CartoStyle);
+
+var refill = extend({
+  stylePath: 'refill-style',
+  version: 11
+}, CartoStyle);
+
+var walkabout = extend({
+  stylePath: 'walkabout-style',
+  version: 7
+}, CartoStyle);
+
+var tron = extend({
+  stylePath: 'tron-style',
+  verseion: 6
+}, CartoStyle);
+
+var zinc = extend({
+  get zincURL () {
+    return getCartoThemeStyleURL(refill, 'color-zinc');
+  },
+  get zincNoLabelConfig () {
+    return getImportObject([refill.basemapURL, this.zincURL, refill.noLabelURL]);
+  },
+  get zincMoreLabelConfig () {
+    return getImportObject([refill.basemapURL, this.zincURL, refill.moreLabelURL]);
+  },
+  get basemapConfig () {
+    return getImportObject([refill.basemapURL, this.zincURL]);
+  }
+}, refill);
+
 var style = {
-  BubbleWrap: 'https://mapzen.com/carto/bubble-wrap-style/8/bubble-wrap-style.zip',
-  BubbleWrapMoreLabels: getImportObject(['https://mapzen.com/carto/bubble-wrap-style/8/bubble-wrap-style.zip', 'https://mapzen.com/carto/bubble-wrap-style/8/themes/label-10.zip']),
-  BubbleWrapNoLabels: getImportObject(['https://mapzen.com/carto/bubble-wrap-style/8/bubble-wrap-style.zip', 'https://mapzen.com/carto/bubble-wrap-style/8/themes/label-0.zip']),
-  Cinnabar: 'https://mapzen.com/carto/cinnabar-style/8/cinnabar-style.zip',
-  CinnabarMoreLabels: getImportObject(['https://mapzen.com/carto/cinnabar-style/8/cinnabar-style.zip', 'https://mapzen.com/carto/cinnabar-style/8/themes/label-10.zip']),
-  CinnabarNoLabels: getImportObject(['https://mapzen.com/carto/cinnabar-style/8/cinnabar-style.zip', 'https://mapzen.com/carto/cinnabar-style/8/themes/label-0.zip']),
-  Refill: 'https://mapzen.com/carto/refill-style/9/refill-style.zip',
-  RefillMoreLabels: getImportObject(['https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/label-10.zip']),
-  RefillNoLabels: getImportObject(['https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/label-0.zip']),
-  Zinc: getImportObject(['https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/color-zinc.zip']),
-  ZincMoreLabels: getImportObject(['https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/color-zinc.zip', 'https://mapzen.com/carto/refill-style/9/themes/label-10.zip']),
-  ZincNoLabels: getImportObject(['https://mapzen.com/carto/refill-style/9/refill-style.zip', 'https://mapzen.com/carto/refill-style/9/themes/color-zinc.zip', 'https://mapzen.com/carto/refill-style/9/themes/label-0.zip']),
-  Walkabout: 'https://mapzen.com/carto/walkabout-style/6/walkabout-style.zip',
-  WalkaboutMoreLabels: getImportObject(['https://mapzen.com/carto/walkabout-style/6/walkabout-style.zip', 'https://mapzen.com/carto/walkabout-style/6/themes/label-10.zip']),
-  WalkaboutNoLabels: getImportObject(['https://mapzen.com/carto/walkabout-style/6/walkabout-style.zip', 'https://mapzen.com/carto/walkabout-style/6/themes/label-0.zip']),
-  Tron: 'https://mapzen.com/carto/tron-style/5/tron-style.zip',
-  TronMoreLabels: getImportObject(['https://mapzen.com/carto/tron-style/5/tron-style.zip', 'https://mapzen.com/carto/tron-style/5/themes/label-10.zip']),
-  TronNoLabels: getImportObject(['https://mapzen.com/carto/tron-style/5/tron-style.zip', 'https://mapzen.com/carto/tron-style/5/themes/label-0.zip'])
+  BubbleWrap: bubblewrap.basemapURL,
+  BubbleWrapMoreLabels: bubblewrap.moreLabelConfig,
+  BubbleWrapNoLabels: bubblewrap.noLabelConfig,
+  Cinnabar: cinnabar.basemapURL,
+  CinnabarMoreLabels: cinnabar.moreLabelConfig,
+  CinnabarNoLabels: cinnabar.noLabelConfig,
+  Refill: refill.basemapURL,
+  RefillMoreLabels: refill.moreLabelConfig,
+  RefillNoLabels: refill.noLabelConfig,
+  Walkabout: walkabout.basemapURL,
+  WalkaboutMoreLabels: walkabout.moreLabelConfig,
+  WalkaboutNoLabels: walkabout.noLabelConfig,
+  Tron: tron.basemapURL,
+  TronMoreLabels: tron.moreLabelConfig,
+  TronNoLabels: tron.noLabelConfig,
+  Zinc: zinc.basemapConfig,
+  ZincMoreLabels: zinc.zincMoreLabelConfig,
+  ZincNoLabels: zinc.zincNoLabelConfig
 };
 
 module.exports = style;
